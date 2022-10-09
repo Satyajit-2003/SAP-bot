ba0429fe0688dcb3d1379371f65a4e19ea074874
Satyajit <satyajit1470@gmail.com>
2022-10-09 08:31:31 +0530

    Making both schedules run

--- clock.py
+++ clock.py
@@ -1,10 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from main import extract_data, send_details, send_warnings
 from config import CONFIG
-sched = BlockingScheduler()
+msg_sched = BlockingScheduler()
+warn_sched = BlockingScheduler()
 
-# @sched.scheduled_job('interval', seconds=10)
-@sched.scheduled_job('interval', hours = int(CONFIG.TIME_INTERVAL))
+# @msg_sched.scheduled_job('interval', seconds=10)
+@msg_sched.scheduled_job('interval', hours = int(CONFIG.TIME_INTERVAL))
 def timed_job():
     print(f'This job is runs every {CONFIG.TIME_INTERVAL} hours and send attendance by Telegram Bot')
     user_list = CONFIG.USERNAME.split(',')
@@ -14,11 +15,13 @@ def timed_job():
         extract_data(user_list[i].strip(), pass_list[i].strip())
         send_details(user_list[i].strip(), chat_id_list[i].strip())
 
-@sched.scheduled_job('cron', hour='16', minute='30')
+@warn_sched.scheduled_job('cron', hour='16', minute='30')
 def scheduled_job():
-    print("This job sends warning message to the user")
+    print("This job sends warning message to the user everyday at 4:30 PM GMT i.e. 10:00 PM IST")
     user_list = CONFIG.USERNAME.split(',')
     chat_id_list = CONFIG.TELEGRAM_CHAT_ID.split(',')
     for i in range(len(user_list)):
         send_warnings(user_list[i].strip(), chat_id_list[i].strip())
-sched.start()
+
+msg_sched.start()
+warn_sched.start()
